120a85b9b079fb74643d29881b830ab91b51c494
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2023-02-05 17:14:30 +0530

    create: btn component

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ children, handleClick }) => {
+	return (
+		<button
+			onClick={handleClick}
+			className="text-white bg-blue-700 hover:bg-blue-800 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 rounded tracking-wide"
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
